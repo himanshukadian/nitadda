d14639efd8dbeeeb9a2a7f836c087f0e1d45f47a
himanshukadian <Himanshu25101998@gmail.com>
2020-09-08 23:09:05 +0530

    Update settings.py

--- nitadda/settings.py
+++ nitadda/settings.py
@@ -69,6 +69,9 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ROOT_URLCONF = 'nitadda.urls'
